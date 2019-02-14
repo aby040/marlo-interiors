519135487d27745efdbf35bb3465f63ef4e008e4
Aby Jose Abraham <abyjoseabraham@yahoo.com>
2019-02-14 17:28:36 +0530

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -12,7 +12,7 @@ time zone offset.
 var current="Winter is here!";   //-->enter what you want the script to display when the target date and time are reached, limit to 20 characters
 var year=2019;    //-->Enter the count down target date YEAR
 var month=2;      //-->Enter the count down target date MONTH
-var day=10;       //-->Enter the count down target date DAY
+var day=28;       //-->Enter the count down target date DAY
 var hour=12;      //-->Enter the count down target date HOUR (24 hour clock)
 var minute=00;    //-->Enter the count down target date MINUTE
 var tz=-5;        //-->Offset for your timezone in hours from UTC (see http://wwp.greenwichmeantime.com/index.htm to find the timezone offset for your location)
